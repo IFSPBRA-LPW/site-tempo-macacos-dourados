925c4ea9665420beecdba057a8c8da1c741d1d73
Marco Antonio <junqueira.marco@aluno.ifsp.edu.br>
2026-03-03 19:42:59 -0300

    novo1

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,25 @@
-import { cityWeather } from "./data.js";
-
-// console.log(cityWeather);
-
+import { cityWeather } from "./data.js"
 
 function banner(city, country, date, icon, temperature){
     const cityClass = document.getElementsByClassName("city")
     cityClass[0].textContent = `${city}, ${country}`
     const dateClass = document.getElementsByClassName("day")
     dateClass[0].textContent = `${date}`
-
-
+    const iconClass = document.getElementsByClassName("icon")
+    iconClass[0].textContent = `${icon}`
+    const temperatureClass = document.getElementsByClassName("temp")
+    temperatureClass[0].textContent = `${temperature}`
 }
 
 function dayInfo(feelsLike, humidity, wind, precipitation){
-
+    const feelsLikeClass = document.getElementsByClassName("fl")
+    const humidityClass = document.getElementsByClassName("hu")
+    const windClass = document.getElementsByClassName("wd")
+    const precipitationClass = document.getElementsByClassName("pc")
+    feelsLikeClass[0].textContent = `${feelsLike}`
+    humidityClass[0].textContent = `${humidity}`
+    windClass[0].textContent = `${wind}`
+    precipitationClass[0].textContent = `${precipitation}`
 }
 
 function daily( day, icon, max, min){
@@ -25,7 +31,7 @@ function hourly(time, temp){
 }
 
 function principal(cityWeather){
-    banner(cityWeather.city,cityWeather.country)
+    banner(cityWeather.city,cityWeather.country,cityWeather.date)
 }
 
 principal(cityWeather)
